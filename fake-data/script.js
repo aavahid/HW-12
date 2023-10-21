e0fea0811f35d4c30cc29cb10b5a56c8b01ae307
vahidaa <vahidaa@code.edu.az>
2023-10-21 23:15:36 +0400

    Home Page added & fake-data added

---
+++ fake-data/script.js
@@ -0,0 +1,100 @@
+const products = [
+  {
+    id: 1,
+    name: "Lenovo NoteBook",
+    imgUrl:
+      "https://www.bakuelectronics.az/assets/cache_image/products/138484/lenovo-156-fhdi3-1215u8gb512gb-ssdfreedosarctic-grey-82rk00purk-n-1_260x204_adf.jpg",
+    price: "799 AZN",
+  },
+  {
+    id: 2,
+    name: "Apple MacBook 13",
+    imgUrl:
+      "https://www.bakuelectronics.az/assets/cache_image/products/93260/apple-macbook-air-13-m1-2020-gold_260x204_adf.jpg",
+    price: "1950 AZN",
+  },
+  {
+    id: 3,
+    name: "Televizor LG",
+    imgUrl:
+      "https://www.bakuelectronics.az/assets/cache_image/products/112253/baku-electronics-%E2%80%94-kopiya_260x220_172.jpg",
+    price: "999 AZN",
+  },
+  {
+    id: 4,
+    name: "Mikser Braun",
+    imgUrl:
+      "https://www.bakuelectronics.az/assets/cache_image/products/96184/braun-hm101ai-hm1010wh-3_260x220_172.jpg",
+    price: "74,99 AZN",
+  },
+  {
+    id: 5,
+    name: "Kondisioner MITSUBISHI",
+    imgUrl:
+      "https://www.bakuelectronics.az/assets/cache_image/products/98207/mitsubishi-srk63zr-w-kit-2_260x220_172.jpg",
+    price: "3399 AZN",
+  },
+  {
+    id: 6,
+    name: "Kondisioner MITSUBISHI",
+    imgUrl:
+      "https://www.bakuelectronics.az/assets/cache_image/products/98207/mitsubishi-srk63zr-w-kit-2_260x220_172.jpg",
+    price: "3399 AZN",
+  },
+  {
+    id: 7,
+    name: "Kondisioner MITSUBISHI",
+    imgUrl:
+      "https://www.bakuelectronics.az/assets/cache_image/products/98207/mitsubishi-srk63zr-w-kit-2_260x220_172.jpg",
+    price: "3399 AZN",
+  },
+  {
+    id: 8,
+    name: "Kondisioner MITSUBISHI",
+    imgUrl:
+      "https://www.bakuelectronics.az/assets/cache_image/products/98207/mitsubishi-srk63zr-w-kit-2_260x220_172.jpg",
+    price: "3399 AZN",
+  },
+  {
+    id: 9,
+    name: "Kondisioner MITSUBISHI",
+    imgUrl:
+      "https://www.bakuelectronics.az/assets/cache_image/products/98207/mitsubishi-srk63zr-w-kit-2_260x220_172.jpg",
+    price: "3399 AZN",
+  },
+  {
+    id: 10,
+    name: "Kondisioner MITSUBISHI",
+    imgUrl:
+      "https://www.bakuelectronics.az/assets/cache_image/products/98207/mitsubishi-srk63zr-w-kit-2_260x220_172.jpg",
+    price: "3399 AZN",
+  },
+  {
+    id: 11,
+    name: "Kondisioner MITSUBISHI",
+    imgUrl:
+      "https://www.bakuelectronics.az/assets/cache_image/products/98207/mitsubishi-srk63zr-w-kit-2_260x220_172.jpg",
+    price: "3399 AZN",
+  },
+  {
+    id: 12,
+    name: "Kondisioner MITSUBISHI",
+    imgUrl:
+      "https://www.bakuelectronics.az/assets/cache_image/products/98207/mitsubishi-srk63zr-w-kit-2_260x220_172.jpg",
+    price: "3399 AZN",
+  },
+  {
+    id: 13,
+    name: "Kondisioner MITSUBISHI",
+    imgUrl:
+      "https://www.bakuelectronics.az/assets/cache_image/products/98207/mitsubishi-srk63zr-w-kit-2_260x220_172.jpg",
+    price: "3399 AZN",
+  },
+  {
+    id: 14,
+    name: "Kondisioner MITSUBISHI",
+    imgUrl:
+      "https://www.bakuelectronics.az/assets/cache_image/products/98207/mitsubishi-srk63zr-w-kit-2_260x220_172.jpg",
+    price: "3399 AZN",
+  },
+];
---
+++ pages/home/script.js
@@ -0,0 +1,45 @@
+const productContainer = document.querySelector(
+  "#product-section > div > .product-container"
+);
+
+products.forEach((p) => {
+  const productDiv = document.createElement("div");
+  productDiv.className = "product";
+  const productImg = document.createElement("img");
+  productImg.src = p.imgUrl;
+  const productName = document.createElement("h3");
+  productName.textContent = p.name;
+  const productPrice = document.createElement("p");
+  productPrice.textContent = p.price;
+  const iconContainer = document.createElement("div");
+  iconContainer.className = "icon-container";
+  const detailIcon = document.createElement("img");
+  detailIcon.src = "../assets/svg/detail.svg";
+  const cartIcon = document.createElement("img");
+  cartIcon.src = "../assets/svg/cart.svg";
+  const favoriteIcon = document.createElement("img");
+  favoriteIcon.src = "../assets/svg/favorite.svg";
+
+  iconContainer.append(cartIcon, favoriteIcon, detailIcon);
+
+  productDiv.append(productImg, productName, productPrice, iconContainer);
+  productContainer.append(productDiv);
+
+  const svgs = iconContainer.querySelectorAll("img");
+  for (let i = 0; i < svgs.length - 1; i++) {
+    svgs[i].style.marginRight = "20px";
+    svgs[i].style.marginTop = "5px";
+  }
+  for (const svg of svgs) {
+    svg.addEventListener("mouseover", () => {
+      svg.style.transform = "scale(2.05)";
+    });
+
+    svg.addEventListener("mouseout", () => {
+      svg.style.transform = "scale(1)";
+      svg.style.boxShadow = "none";
+    });
+  }
+
+  iconContainer.style.gridTemplateColumns = "repeat(2, 1fr)";
+});
